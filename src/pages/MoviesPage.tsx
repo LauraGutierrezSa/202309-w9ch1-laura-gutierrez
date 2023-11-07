baa879c4d4133d292b661ee84dc72525a6744180
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-07 16:02:31 +0100

    Add dispatcher hooksImplement useAppDispatch in MovieList

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,8 +1,28 @@
+import { useDispatch } from "react-redux";
+import useMoviesApi from "../hooks/useMovieApi";
+import { loadMoviesActionCreator } from "../store/movieSlice";
+import { useEffect } from "react";
+import MovieList from "../components/MovieList/MovieList";
+
 const MoviesPage = (): React.ReactElement => {
+  const { getMovies } = useMoviesApi();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const currentMovies = await getMovies();
+      dispatch(loadMoviesActionCreator(currentMovies));
+    })();
+  }, [dispatch, getMovies]);
+
   return (
-    <div className="films__container">
-      <h1 className="films__title">Mis peliculas favoritas</h1>
-    </div>
+    <>
+      <header className="films__container">
+        <h1 className="films__title">Mis peliculas favoritas</h1>
+      </header>
+      <MovieList />
+    </>
   );
 };
 
