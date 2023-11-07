02111a5866369e7590494594dcf117af705f0764
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-07 12:20:47 +0100

    Add Homepage component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Films from "../../pages/homepage";
 
 const App = (): React.ReactElement => (
   <div className="main-container">
     <Routes>
-      <Route path="/films" />
+      <Route path="/films" element={<Films />} />
       <Route path="/" element={<Navigate to="/films" />} />
     </Routes>
   </div>
