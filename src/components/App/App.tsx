a53cca479b5cec0b73eb4cf830692bdce6b87637
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-08 06:26:54 +0100

    Add AppStyled file into App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import MoviesPage from "../../pages/MoviesPage";
+import AppStyled from "./AppStyled";
 
 const App = (): React.ReactElement => (
-  <div className="main-container">
+  <AppStyled className="main-container">
     <Routes>
       <Route path="/films" element={<MoviesPage />} />
       <Route path="/" element={<Navigate to="/films" />} />
     </Routes>
-  </div>
+  </AppStyled>
 );
 
 export default App;
